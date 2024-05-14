604f1b679a741551b18c7bccf1a49228509174d1
Andrei Shcheglov <nordazavr@gmail.com>
2024-05-14 19:18:44 +0300

    add logger

--- app/main.py
+++ app/main.py
@@ -1,20 +1,28 @@
 from flask import Flask, render_template, send_from_directory
 import re
 import os
+import logging
+
+# Настройка логгера
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s',
+                    handlers=[
+                        logging.FileHandler("app.log"),
+                        logging.StreamHandler()
+                    ])
+logger = logging.getLogger(__name__)
 
 def getUsers():
-    # Открыть файл extensions.conf и прочитать его содержимое
-    with open('extensions.conf', 'r', encoding='utf-8') as f:
-        data = f.read()
-        # print(data)
+    logger.info("Чтение файла extensions.conf")
+    try:
+        with open('extensions.conf', 'r', encoding='utf-8') as f:
+            data = f.read()
+    except FileNotFoundError:
+        logger.error("Файл extensions.conf не найден")
+        return []
 
-    # Паттерн для извлечения данных
     pattern = r'\$\[\"\$\{CALLERID\(num\)\}\" = "(.+)"\]\?(line\d+)'
-
-    # Извлечение данных из строки
     matches = re.findall(pattern, data)
-
-    # Создание списка словарей
     users = []
     for match in matches:
         telephone = match[0]
@@ -22,12 +30,13 @@ def getUsers():
         fio = data.split(telephone)[-1].split('\n')[0].split(';')[-1].strip()
         counter = 0
         try:
-            # Посчитаем кол-во записей
             with open('home/' + folder + '/counter.txt', 'r') as file:
                 counter = file.read()
                 counter = int(counter) - 1
-        except:
-            pass
+        except FileNotFoundError:
+            logger.warning(f"Файл counter.txt не найден для папки {folder}")
+        except ValueError:
+            logger.warning(f"Неверное значение в файле counter.txt для папки {folder}")
 
         users.append({'telephone': telephone, 'fio': fio, 'folder': folder, 'counter': counter})
 
@@ -38,7 +47,8 @@ def getUser(id):
     for user in users:
         if user.get('folder') == id:
             return user
-
+    logger.warning(f"Пользователь с id {id} не найден")
+    return None
 
 app = Flask(__name__)
 
@@ -48,27 +58,26 @@ def index():
     users = getUsers()
     return render_template('index.html', users=users)
 
-
 @app.route('/user/<id>')
 def user(id):
     user = getUser(id)
+    if not user:
+        logger.error(f"Пользователь с id {id} не найден")
+        return "Пользователь не найден", 404
     recordings = []
     try:
-        with open('home/' + id + '/corpus.txt', 'r', encoding='utf-8') as f:
+        with open(f'home/{id}/corpus.txt', 'r', encoding='utf-8') as f:
             for i, line in enumerate(f, start=1):
                 line = line.strip()
                 if line:
                     try:
                         filename, name = line.split(';', maxsplit=1)
                         if name and filename:
-                            record = {'id': i, 'name': name, 'filename': filename}
-                            recordings.append(record)
-                    except:
-                        record = {'id': i, 'name': line, 'filename': ''}
-                        recordings.append(record)
-    except:
-        pass
-
+                            recordings.append({'id': i, 'name': name, 'filename': filename})
+                    except ValueError:
+                        recordings.append({'id': i, 'name': line, 'filename': ''})
+    except FileNotFoundError:
+        logger.warning(f"Файл corpus.txt не найден для пользователя {id}")
 
     return render_template('user.html', recordings=recordings, user=user)
 
@@ -76,14 +85,10 @@ def user(id):
 def serve_record(folder, filename):
     return send_from_directory(f'./home/{folder}/records', filename)
 
-
 @app.route('/delete-audio/<user_folder>/<filename>')
 def delete_audio(user_folder, filename):
     try:
-        # Удаляем аудиофайл
         os.remove(os.path.join('home', user_folder, 'records', filename))
-
-        # Удаляем строку с именем файла и сохраняем ее содержимое
         with open(os.path.join('home', user_folder, 'corpus.txt'), 'r', encoding='utf-8') as f:
             lines = f.readlines()
 
@@ -92,36 +97,28 @@ def delete_audio(user_folder, filename):
         for line in lines:
             if line.strip() and not line.startswith(filename):
                 corpus_content += line
-                print(f'{line} add to corpus_content')
+                logger.debug(f'{line} добавлено в corpus_content')
             else:
                 corpus_content_deleted = line.strip().split(';')[1] + '\n'
-                print(f'{line} add to corpus_content_deleted')
+                logger.debug(f'{line} добавлено в corpus_content_deleted')
 
-        # Записываем новую строку в corpus.txt без имени файла
         with open(os.path.join('home', user_folder, 'corpus.txt'), 'w', encoding='utf-8') as f:
             f.write(corpus_content)
             f.write(corpus_content_deleted)
 
-        # Обновляем corpus.txt и подсчитываем количество точек с запятой
         semicolon_count = 1
         with open(os.path.join('home', user_folder, 'corpus.txt'), 'r', encoding='utf-8') as f:
             for line in f:
                 semicolon_count += line.count(';')
 
-        # Записываем количество точек с запятой в файл counter.txt
         with open(os.path.join('home', user_folder, 'counter.txt'), 'w') as f:
             f.write(str(semicolon_count))
 
-
-        # Возвращаем ответ 204 No Content в случае успеха
+        logger.info(f"Аудиофайл {filename} успешно удален для пользователя {user_folder}")
         return '', 204
     except Exception as e:
-        print('delete_audio:', str(e))
-        # Возвращаем ответ 500 Internal Server Error в случае ошибки
+        logger.error(f'Ошибка при удалении аудиофайла: {e}')
         return '', 500
 
-
-
 if __name__ == '__main__':
-    # app.static_folder = '.'
     app.run(debug=True, host="0.0.0.0", port="8080")
